43ee8980fb3ccc41e32584f0d1adf6d7cb9095bf
dyong <likedyong@gmail.com>
2015-12-25 14:59:54 +0900

    Guestbook controller 구현  - 방명록 보기(viewGuestbook) -> 방명록 보기(guestbookList) 메서드 제거  - 방명록 생성(createGuestbook)  - 방명록 수정(updateGuestbook)

--- src/main/java/com/zoel/controllers/GuestbookController.java
+++ src/main/java/com/zoel/controllers/GuestbookController.java
@@ -4,27 +4,81 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.zoel.exceptions.ConflictException;
+import com.zoel.exceptions.DBWriteFailureException;
+import com.zoel.exceptions.GuestbookNotFoundException;
 import com.zoel.services.GuestbookService;
+import com.zoel.util.EmailHelper;
 import com.zoel.vo.Guestbook;
 
 @Controller
 @RequestMapping("/guestbooks")
 public class GuestbookController {
-	
+
 	@Autowired
 	GuestbookService guestbookService;
-	
-	@RequestMapping
-	public ModelAndView guestbookList(){
+
+	EmailHelper emailHelper;
+
+	public GuestbookController() {
+		this.emailHelper = new EmailHelper();
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ModelAndView viewGuestbook() {
 		List<Guestbook> guestbooks = guestbookService.getAllGuestbooks();
-		
+
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("guestbooks", guestbooks);
-		mv.setViewName("guestbookList");
-		
+		mv.setViewName("guestbook");
+
+		return mv;
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ModelAndView createGuestbook(@ModelAttribute Guestbook param) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/guestbooks");
+
+		boolean bValidEmail = this.emailHelper.validate(param.getEmail());
+		if (bValidEmail) {
+			try{
+				guestbookService.createGuestbook(param);
+			} catch(DBWriteFailureException e){
+				throw new ConflictException();
+			}
+		} else {
+			throw new ConflictException();			
+		}
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/{guestbookId}", method = RequestMethod.POST)
+	public ModelAndView updateGuestbook(@PathVariable Long guestbookId, @ModelAttribute Guestbook param) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/guestbooks");
+
+		param.setId(guestbookId);
+
+		boolean bValidAuth = guestbookService.validateOwner(param);
+		if (bValidAuth) {
+			try {
+				guestbookService.updateGuestbook(param);
+			} catch (DBWriteFailureException e) {
+				throw new ConflictException();
+			}
+		} else {
+			throw new GuestbookNotFoundException();
+		}
+
 		return mv;
 	}
 }
